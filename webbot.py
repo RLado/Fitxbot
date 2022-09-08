d273798bf03baa81e1522a821ed42f755f377728
Ricard Lado <ricardlado@hotmail.com>
2022-09-08 08:48:18 +0200

    Fixed to work with selenium 4.3.0 and up. - "Selenium 4.3.0 * Deprecated find_element_by_* and find_elements_by_* are now removed (#10712) * Deprecated Opera support has been removed (#10630) * Fully upgraded from python 2x to 3.7 syntax and features (#10647) * Added a devtools version fallback mechanism to look for an older version when mismatch occurs (#10749) * Better support for co-operative multi inheritance by utilising super() throughout * Improved type hints throughout" - Timeout modification to allow raspberry pi enough time

--- webbot.py
+++ webbot.py
@@ -1,45 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
 
 
 url='https://w3.cezanneondemand.com/CezanneOnDemand/Account/LogIn'
 
-def clock_in(user,passwd,timeout=15):
+def clock_in(user,passwd,timeout=120):
     #Set firefox to headless mode
     ffopt=webdriver.FirefoxOptions()
     ffopt.headless=True
 
     #Create web driver object and go to the URL
-    browser=webdriver.Firefox(executable_path='./geckodriver',options=ffopt)
+    browser=webdriver.Firefox(options=ffopt)
     browser.get(url)
 
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'//button[@type="submit"]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
 
     #Write username and password
-    browser.find_element_by_id('Username').clear()
-    browser.find_element_by_id('Username').send_keys(user)
-    browser.find_element_by_id('Password').clear()
-    browser.find_element_by_id('Password').send_keys(passwd)
+    browser.find_element(By.ID, 'Username').clear()
+    browser.find_element(By.ID, 'Username').send_keys(user)
+    browser.find_element(By.ID, 'Password').clear()
+    browser.find_element(By.ID, 'Password').send_keys(passwd)
     #Log in
-    browser.find_element_by_xpath('//button[@type="submit"]').click()
+    browser.find_element(By.XPATH, '//button[@type="submit"]').click()
 
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'//*[@title="Haz clic para registrar tu Clock In"]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
 
     #Click on the clock in button
-    browser.find_element_by_xpath('//*[@title="Haz clic para registrar tu Clock In"]').click()
+    browser.find_element(By.XPATH, '//*[@title="Haz clic para registrar tu Clock In"]').click()
     
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'(//button[@type="button"])[2]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
     
     #Confirm
-    browser.find_element_by_xpath('(//button[@type="button"])[2]').click()
+    browser.find_element(By.XPATH, '(//button[@type="button"])[2]').click()
 
     #Quit the browser
     browser.close()
@@ -48,40 +49,40 @@ def clock_in(user,passwd,timeout=15):
     #Print log message
     print(f'Clocked in at: {str(datetime.datetime.now())}')
 
-def clock_out(user,passwd,timeout=15):
+def clock_out(user,passwd,timeout=120):
     #Set firefox to headless mode
     ffopt=webdriver.FirefoxOptions()
     ffopt.headless=True
 
     #Create web driver object and go to the URL
-    browser=webdriver.Firefox(executable_path='./geckodriver',options=ffopt)
+    browser=webdriver.Firefox(options=ffopt)
     browser.get(url)
 
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'//button[@type="submit"]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
 
     #Write username and password
-    browser.find_element_by_id('Username').clear()
-    browser.find_element_by_id('Username').send_keys(user)
-    browser.find_element_by_id('Password').clear()
-    browser.find_element_by_id('Password').send_keys(passwd)
+    browser.find_element(By.ID, 'Username').clear()
+    browser.find_element(By.ID, 'Username').send_keys(user)
+    browser.find_element(By.ID, 'Password').clear()
+    browser.find_element(By.ID, 'Password').send_keys(passwd)
     #Log in
-    browser.find_element_by_xpath('//button[@type="submit"]').click()
+    browser.find_element(By.XPATH, '//button[@type="submit"]').click()
 
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'//*[@title="Haz clic para el Clock Out"]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
 
     #Click on the clock in button
-    browser.find_element_by_xpath('//*[@title="Haz clic para el Clock Out"]').click()
+    browser.find_element(By.XPATH, '//*[@title="Haz clic para el Clock Out"]').click()
 
     #Wait for page to load
     element_present=EC.presence_of_element_located((By.XPATH,'(//button[@type="button"])[2]'))
-    webdriver.support.ui.WebDriverWait(browser,timeout).until(element_present)
+    WebDriverWait(browser,timeout).until(element_present)
 
     #Confirm
-    browser.find_element_by_xpath('(//button[@type="button"])[2]').click()
+    browser.find_element(By.XPATH, '(//button[@type="button"])[2]').click()
 
     #Quit the browser
     browser.close()
